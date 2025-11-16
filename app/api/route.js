994ebc4fa11e066c39665462c86f021d1aa10e9d
datapacket872 <ID2863751345>
2025-11-16 11:27:40 +0000

    Fix inngest imports and route setup

--- app/api/route.js
+++ app/api/route.js
@@ -1,15 +1,17 @@
 import { serve } from "inngest/next";
-import client from './inngest/client'
+import { inngest } from "./inngest/client";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "./inngest/functions"; 
+// or wherever your functions actually live
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
   ],
 });
+
 
 
 
